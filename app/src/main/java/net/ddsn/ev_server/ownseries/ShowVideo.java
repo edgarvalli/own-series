b247806d865cefa64e7914973447a8a9c869776b
Edgar Valli <edgarvalli80@gmail.com>
2018-11-09 23:46:55 -0600

    series

--- app/src/main/java/net/ddsn/ev_server/ownseries/ShowVideo.java
+++ app/src/main/java/net/ddsn/ev_server/ownseries/ShowVideo.java
@@ -5,13 +5,16 @@ import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class ShowVideo extends AppCompatActivity {
 
     VideoView video;
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,34 @@ public class ShowVideo extends AppCompatActivity {
         Intent i = getIntent();
 
         video = findViewById(R.id.video);
+        back = findViewById(R.id.back_arrow_chapters);
+        back.setVisibility(View.INVISIBLE);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         Uri uri = Uri.parse(i.getStringExtra("url"));
         video.setVideoURI(uri);
 
-        MediaController mc = new MediaController(this);
+        MediaController mc;
+        mc = new MediaController(this) {
+
+            @Override
+            public void hide() {
+                back.setVisibility(View.INVISIBLE);
+                super.hide();
+            }
+
+            @Override
+            public void show() {
+                back.setVisibility(View.VISIBLE);
+                super.show();
+            }
+        };
         video.setMediaController(mc);
         mc.setAnchorView(video);
 
